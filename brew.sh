6698d12e78688842207aa6e7cc78efa6f4418f77
Russ Kociuba <russell.kociuba@tr.com>
2017-05-26 12:09:33 -0400

    More updates

--- brew.sh
+++ brew.sh
@@ -42,8 +42,8 @@ brew install wget --with-iri
 
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
+brew install grep
+brew install openssh
 # brew install homebrew/dupes/screen
 # brew install homebrew/php/php56 --with-gmp
 
